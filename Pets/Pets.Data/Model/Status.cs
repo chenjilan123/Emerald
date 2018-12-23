38a10225bf86b0f389c5c94e6236c810fa0880ef
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2018-12-23 15:57:53 +0800

    1

---
+++ Pets/Pets.Data/Model/Status.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets.Data.Model
+{
+    public enum Status
+    {
+        OK = StatusCodes.Status200OK,
+        Created = StatusCodes.Status201Created,
+        Accepted = StatusCodes.Status202Accepted,
+        BadRequest = StatusCodes.Status400BadRequest,
+        NotFound = StatusCodes.Status404NotFound,
+        
+    }
+}
