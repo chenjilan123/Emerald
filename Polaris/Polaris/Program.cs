3c8fe9f28ebc3f26820bdaeaaa0501cfb806ee9b
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-01-15 23:07:47 +0800

    1

--- Polaris/Polaris/Program.cs
+++ Polaris/Polaris/Program.cs
@@ -16,6 +16,10 @@ namespace Polaris
 {
     public class Program
     {
+        /// <summary>
+        /// Scoped services are disposed by the container that created them. If a scoped service is created in the root container, the service's lifetime is effectively promoted to singleton because it's only disposed by the root container when app/server is shut down. 
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             //http或https均可启动，https为不安全连接
@@ -25,23 +29,51 @@ namespace Polaris
             //    host.WaitForShutdown();
             //}
 
+
+            //using (var host = WebHost.Start("http://localhost:8080", router => router
+            //    .MapGet("hello/{name}", (req, res, data) =>
+            //        res.WriteAsync($"Hello, {data.Values["name"]}!"))
+            //    .MapGet("buenosdias/{name}", (req, res, data) =>
+            //        res.WriteAsync($"Buenos dias, {data.Values["name"]}!"))
+            //    .MapGet("throw/{message?}", (req, res, data) =>
+            //        throw new Exception((string)data.Values["message"] ?? "Uh oh!")) //Status Code : 500
+            //    .MapGet("{greeting}/{name}", (req, res, data) =>
+            //        res.WriteAsync($"{data.Values["greeting"]}, {data.Values["name"]}!"))
+            //    .MapGet("", (req, res, data) => res.WriteAsync("Hello, World!"))))
+            //{
+            //    Console.WriteLine("Use Control-C to show down the host......");
+            //    host.WaitForShutdown();
+            //}
+
             IApplicationBuilder app;
             IRouteBuilder router1;
-            
-            using (var host = WebHost.Start("http://localhost:8080", router => router
-                .MapGet("hello/{name}", (req, res, data) =>
-                    res.WriteAsync($"Hello, {data.Values["name"]}!"))
-                .MapGet("buenosdias/{name}", (req, res, data) =>
-                    res.WriteAsync($"Buenos dias, {data.Values["name"]}!"))
-                .MapGet("throw/{message?}", (req, res, data) =>
-                    throw new Exception((string)data.Values["message"] ?? "Uh oh!")) //Status Code : 500
-                .MapGet("{greeting}/{name}", (req, res, data) =>
-                    res.WriteAsync($"{data.Values["greeting"]}, {data.Values["name"]}!"))
-                .MapGet("", (req, res, data) => res.WriteAsync("Hello, World!"))))
+
+            IApplicationLifetime appLifetime = null;
+            appLifetime.ApplicationStarted.Register(OnStarted);
+            appLifetime.ApplicationStopped.Register(OnStopping);
+            appLifetime.ApplicationStopped.Register(OnStopped);
+
+            Console.CancelKeyPress += (sender, eventArgs) =>
             {
-                Console.WriteLine("Use Control-C to show down the host......");
-                host.WaitForShutdown();
-            }
+                appLifetime.StopApplication();
+                // Don't terminate the process immediately, wait for the Main thread to exit gracefully.
+                eventArgs.Cancel = true;
+            };
+        }
+
+        private static void OnStopped()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void OnStopping()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void OnStarted()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Polaris/Polaris/Showdown.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Polaris
+{
+    public class Showdown
+    {
+        private readonly IApplicationLifetime _appLifetime;
+
+        public Showdown(IApplicationLifetime appLifetime)
+        {
+            _appLifetime = appLifetime;
+        }
+
+        public void Shutdown()
+        {
+            _appLifetime.StopApplication();
+        }
+    }
+}
+}
