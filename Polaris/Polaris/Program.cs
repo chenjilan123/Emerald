5b1c32b9a060dab65c346649e7a8c630098d32ce
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-01-15 21:48:11 +0800

    1

--- Polaris/Polaris/Program.cs
+++ Polaris/Polaris/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -16,9 +17,26 @@ namespace Polaris
     {
         public static void Main(string[] args)
         {
-            using (var host = WebHost.Start("http://localhost:8080", app => app.Response.WriteAsync("Hello World!")))
+            //http或https均可启动，https为不安全连接
+            //using (var host = WebHost.Start("https://localhost:8080", app => app.Response.WriteAsync("Hello World!")))
+            //{
+            //    Console.WriteLine("Use Ctrl-C to shutdown the host...");
+            //    host.WaitForShutdown();
+            //}
+
+            //IRouteBuilder
+            using (var host = WebHost.Start("http://localhost:8080", router => router
+                .MapGet("hello/{name}", (req, res, data) =>
+                    res.WriteAsync($"Hello, {data.Values["name"]}!"))
+                .MapGet("buenosdias/{name}", (req, res, data) =>
+                    res.WriteAsync($"Buenos dias, {data.Values["name"]}!"))
+                .MapGet("throw/{message?}", (req, res, data) =>
+                    throw new Exception((string)data.Values["message"] ?? "Uh oh!")) //Status Code : 500
+                .MapGet("{greeting}/{name}", (req, res, data) =>
+                    res.WriteAsync($"{data.Values["greeting"]}, {data.Values["name"]}!"))
+                .MapGet("", (req, res, data) => res.WriteAsync("Hello, World!"))))
             {
-                Console.WriteLine("Use Ctrl-C to shutdown the host...");
+                Console.WriteLine("Use Control-C to show down the host......");
                 host.WaitForShutdown();
             }
         }
