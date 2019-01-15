9a2b8e4d80273a5aa9dc6853803494ff982f8602
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2019-01-15 20:59:14 +0800

    1

--- Polaris/Polaris/Program.cs
+++ Polaris/Polaris/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -15,61 +16,11 @@ namespace Polaris
     {
         public static void Main(string[] args)
         {
-            //无法运行
-            //var webHost = new WebHostBuilder()
-            //    .UseKestrel()
-            //    .UseContentRoot(Directory.GetCurrentDirectory())
-            //    .ConfigureAppConfiguration((hostingContext, config) =>
-            //    {
-            //        var env = hostingContext.HostingEnvironment;
-            //        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-            //              .AddJsonFile($"appsettings.{env.EnvironmentName}.json",
-            //                  optional: true, reloadOnChange: true);
-            //        config.AddEnvironmentVariables();
-            //    })
-            //    .ConfigureLogging((hostingContext, logging) =>
-            //    {
-            //        var section = hostingContext.Configuration.GetSection("Logging");
-            //        logging.AddConfiguration(section);
-            //        logging.AddConsole();
-            //        logging.AddDebug();
-            //        logging.AddEventSourceLogger();
-            //    })
-            //    .UseStartup<Startup>()
-            //    .Build();
-            //webHost.Run();
-
-
-            var host = CreateWebHostBuilder(args).Build();
-            var logger = host.Services.GetRequiredService<ILogger<Program>>();
-            //Order 2
-            logger.LogInformation("Application running");
-
-            host.Run();
+            using (var host = WebHost.Start("http://localhost:8080", app => app.Response.WriteAsync("Hello World!")))
+            {
+                Console.WriteLine("Use Ctrl-C to shutdown the host...");
+                host.WaitForShutdown();
+            }
         }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    var env = hostingContext.HostingEnvironment;
-                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                            .AddJsonFile($"appsettings.{env.EnvironmentName}.json",
-                                optional: true, reloadOnChange: true);
-                    config.AddEnvironmentVariables();
-                })
-                .UseStartup<Startup>()
-                .ConfigureLogging((hostingContext, logging) =>
-                {
-                    //Clear default providers
-                    logging.ClearProviders();
-                    //ILoggingProvider
-                    var section = hostingContext.Configuration.GetSection("Logging");
-                    logging.AddConfiguration(section);
-                    logging.AddConsole();
-                    logging.AddDebug();
-                    //ASP.NET Core 2.2
-                    logging.AddEventSourceLogger();
-                });
     }
 }
